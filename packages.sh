68a84a6086ca12368c429c3d59ca6e5d582ad030
biopharma <41162517+biopharma@users.noreply.github.com>
2018-07-17 11:33:08 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -14,12 +14,12 @@ apt-get install \
      ca-certificates \
      curl \
      gnupg2 \
-     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
+     software-properties-common;
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -;
+apt-key fingerprint 0EBFCD88;
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce
+   stable";
+apt-get update;
+apt-get install docker-ce;
